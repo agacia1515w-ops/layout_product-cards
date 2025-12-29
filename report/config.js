06661cf14a623e0dd669b7e4c0271638b1ea2511
agacia1515w-ops <agacia1515w@gmail.com>
2025-12-29 16:49:45 +0100

    Add BackstopJS test report

---
+++ report/config.js
@@ -0,0 +1,144 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/layout_product_cards_Card_component_0_data-qacard_0_desktop.png",
+        "test": "../bitmaps_test/20251221-192230/layout_product_cards_Card_component_0_data-qacard_0_desktop.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "layout_product_cards_Card_component_0_data-qacard_0_desktop.png",
+        "label": "Card component",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "file:///Users/mariuszstasiak/layout_product-cards/src/index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/layout_product_cards_Card_component_0_data-qacard_1_tablet.png",
+        "test": "../bitmaps_test/20251221-192230/layout_product_cards_Card_component_0_data-qacard_1_tablet.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "layout_product_cards_Card_component_0_data-qacard_1_tablet.png",
+        "label": "Card component",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "file:///Users/mariuszstasiak/layout_product-cards/src/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/layout_product_cards_Card_component_0_data-qacard_2_phone.png",
+        "test": "../bitmaps_test/20251221-192230/layout_product_cards_Card_component_0_data-qacard_2_phone.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "layout_product_cards_Card_component_0_data-qacard_2_phone.png",
+        "label": "Card component",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "file:///Users/mariuszstasiak/layout_product-cards/src/index.html",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/layout_product_cards_Link_hover_0_data-qahover_0_desktop.png",
+        "test": "../bitmaps_test/20251221-192230/layout_product_cards_Link_hover_0_data-qahover_0_desktop.png",
+        "selector": "[data-qa=\"hover\"]",
+        "fileName": "layout_product_cards_Link_hover_0_data-qahover_0_desktop.png",
+        "label": "Link hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "file:///Users/mariuszstasiak/layout_product-cards/src/index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/layout_product_cards_Link_hover_0_data-qahover_1_tablet.png",
+        "test": "../bitmaps_test/20251221-192230/layout_product_cards_Link_hover_0_data-qahover_1_tablet.png",
+        "selector": "[data-qa=\"hover\"]",
+        "fileName": "layout_product_cards_Link_hover_0_data-qahover_1_tablet.png",
+        "label": "Link hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "file:///Users/mariuszstasiak/layout_product-cards/src/index.html",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/layout_product_cards_Link_hover_0_data-qahover_2_phone.png",
+        "test": "../bitmaps_test/20251221-192230/layout_product_cards_Link_hover_0_data-qahover_2_phone.png",
+        "selector": "[data-qa=\"hover\"]",
+        "fileName": "layout_product_cards_Link_hover_0_data-qahover_2_phone.png",
+        "label": "Link hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "file:///Users/mariuszstasiak/layout_product-cards/src/index.html",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "layout_product_cards"
+});
