f60fa2b136c5d6d464fc914d07f0ecb378934a2f
agacia1515w-ops <agacia1515w@gmail.com>
2026-01-02 11:39:06 +0100

    Restore demo build

---
+++ docs/layout_product-cards.ab9a05f4.js
@@ -0,0 +1,2 @@
+document.getElementById("product-img").src={};let e=document.querySelector(".stars"),t=e.querySelectorAll(".stars__star");parseInt(e.className.match(/stars--(\d)/)[1],10),t.forEach((e,t)=>{e.style.backgroundImage=`url(${{}})`});
+//# sourceMappingURL=layout_product-cards.ab9a05f4.js.map
