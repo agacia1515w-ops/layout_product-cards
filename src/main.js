79307f6c86817dcf8c2c20337d5940532d11292c
agacia1515w-ops <agacia1515w@gmail.com>
2026-01-02 11:23:21 +0100

    Deploy product card demo to gh-pages

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-'use strict';
+// importujemy obrazki Parcel-em
+import imacImg from './images/imac.jpeg';
+import starActive from './images/star-active.svg';
+import starInactive from './images/star.svg';
+
+// ustawiamy główny obrazek
+document.getElementById('product-img').src = imacImg;
+
+// ustawiamy gwiazdki
+const starsContainer = document.querySelector('.stars');
+const stars = starsContainer.querySelectorAll('.stars__star');
+const rating = parseInt(starsContainer.className.match(/stars--(\d)/)[1], 10);
+
+stars.forEach((star, index) => {
+  star.style.backgroundImage = index < rating ? `url(${starActive})` : `url(${starInactive})`;
+});
